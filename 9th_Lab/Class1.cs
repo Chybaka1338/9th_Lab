9a295d247aec1e2d5172d62a26578dbc04170742
Chybaka1337 <zommer.03@gmail.com>
2023-03-19 22:49:38 +0300

    Добавьте файлы проекта.

---
+++ 9th_Lab/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9th_Lab
+{
+    internal class Class1
+    {
+        public static string path = @".\persons.txt";
+
+        public static void Me()
+        {
+            File.Delete(@".\persons.txt");
+            Console.WriteLine(File.Exists(@".\persons.txt"));
+        }
+    }
+}
---
+++ 9th_Lab/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9th_Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1.Me();
+            Console.ReadKey();
+
+            
+        }
+    }
+}
