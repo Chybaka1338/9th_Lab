fb694664ee02b69033d27650f30e31e12d320d41
Chybaka1337 <zommer.03@gmail.com>
2023-03-26 18:34:37 +0300

    add task1 and task4, add paths to db

--- 9th_Lab/Program.cs
+++ 9th_Lab/Program.cs
@@ -4,9 +4,22 @@ namespace _9th_Lab
 {
     public class Program
     {
+        static string[] task1_DB = new string[] {
+            @"C:\Users\kakaw\source\repos\9th_Lab\9th_Lab\Task1DB\Group1.txt",
+            @"C:\Users\kakaw\source\repos\9th_Lab\9th_Lab\Task1DB\Group2.txt",
+            @"C:\Users\kakaw\source\repos\9th_Lab\9th_Lab\Task1DB\Group3.txt",
+        };
+
+        static string[] task4_DB = new string[]
+        {
+            @"C:\Users\kakaw\source\repos\9th_Lab\9th_Lab\Task4DB\Group1.txt",
+            @"C:\Users\kakaw\source\repos\9th_Lab\9th_Lab\Task4DB\Group2.txt",
+        };
+
         static void Main()
         {
-            Task4.Run();
+            Task1.Run(task1_DB);
+            Task4.Run(task4_DB);
             Console.ReadKey();
         }
     }
